1f22e93e3177002079ba280e679b012b6405d56e
andre <andreolamego@gmail.com>
2025-12-09 01:28:25 -0300

    adiciona estrutura inicial do projeto spring

---
+++ src/test/java/com/lamego/agendapro/AgendaProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lamego.agendapro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaProApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
